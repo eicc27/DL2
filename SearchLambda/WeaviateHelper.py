af095831c1a72e9354d3e626adaac1db77844918
Jiseshen <1658288901@qq.com>
2023-07-22 17:43:45 +0800

    Search Lambda

--- SearchLambda/WeaviateHelper.py
+++ SearchLambda/WeaviateHelper.py
@@ -2,8 +2,8 @@ import weaviate
 
 
 def WeaviateHelper:
-    def __init__(self, url="https://dl2-6ld8p0k2.weaviate.network", key="ugyXUyDB0ODDjlivf1URI9lQBCQEc1t8orke", ):
-        self.client = weaviate.Client(url=url, auth_client_secret=weaviate.AuthApiKey(api_key=key), additional_headers={'X-HuggingFace-Api-Key':"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"})
+    def __init__(self, url="https://dl2-6ld8p0k2.weaviate.network", key="ugyXUyDB0ODDjlivf1URI9lQBCQEc1t8orke", api_key="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"):
+        self.client = weaviate.Client(url=url, auth_client_secret=weaviate.AuthApiKey(api_key=key), additional_headers={'X-HuggingFace-Api-Key': api_key})
 
     def getNear(self, query):
         return self.client.query.get("Abstract",["arxiv_id"]).with_near_text({"concepts": [query]}).with_limit(20).do()
