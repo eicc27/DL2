165858ce62f55e1edcb56298d56d368a405ef679
Jiseshen <1658288901@qq.com>
2023-07-22 17:35:52 +0800

    Search Lambda

---
+++ SearchLambda/MysqlHelper.py
@@ -0,0 +1,103 @@
+import mysql.connector as connector
+from mysql.connector.cursor import RowType
+
+class MysqlHelper:
+    def __init__(self,
+                 url = 'database-test.cb6gwcrbia4j.us-east-1.rds.amazonaws.com',
+                 user = 'admin',
+                 pwd = 'Jise246808642',
+                 db = 'dl2') -> None:
+        self.conn = connector.connect(
+            host = url,
+            user = user,
+            password = pwd,
+            database = db)
+        self.conn.autocommit = False # disable autocommit
+        self.cursor = self.conn.cursor()
+
+    def close(self) -> None:
+        self.conn.commit()
+        self.conn.close()
+
+    def _getPaperByTitle(self, query: str) -> list[RowType]:
+        params = (query,)
+        self.cursor.execute(
+            """
+                select * from paper
+                    where title like
+                        CONCAT('%', %s, '%')
+                order by citations desc, year desc
+                limit 10;
+            """,
+            params)
+        return self.cursor.fetchall()
+
+    def _getPaperByArxivId(self, arxivId: str) -> RowType:
+        params = (arxivId,)
+        self.cursor.execute(
+            'SELECT * FROM paper WHERE arxiv_id = %s',
+            params)
+        return self.cursor.fetchone()
+
+    def _getPaperRespByArxivId(self, arxivId: str):
+        paper = self._getPaperByArxivId(arxivId)
+        if paper is None:
+            return None
+        tasks = self._getMostPopularTasksByNumOfPapers(arxivId)
+        methods = self._getMostPopularMethodsByNumOfPapers(arxivId)
+        authors = self._getAuthorPapersByPaperId(arxivId)
+        codes = self._getCodesByPaperId(arxivId)
+        return {
+            'arxivId': arxivId,
+            'title': paper[1],
+            'abs': paper[2],
+            'citations': paper[3],
+            'authors': [a[0] for a in authors],
+            'tasks': {
+                'taskName': [t[0] for t in tasks],
+                'numPapers': [t[1] for t in tasks]
+            },
+            'methods': {
+                'methodName': [m[0] for m in methods],
+                'numPapers': [m[1] for m in methods]
+            },
+            'codes': [{
+                'url': c[1],
+                'rating': c[0]
+            } for c in codes],
+        }
+
+    def getPaperByArxivIds(self, arxivIds: list[str]):
+        try:
+            return [self._getPaperRespByArxivId(arxivId) for arxivId in arxivIds]
+        except connector.Error as err:
+            print(err)
+            return None
+        finally:
+            self.conn.commit()
+            self.conn.close()
+
+    def getPaperByArxivId(self, arxivId: str):
+        try:
+            return self._getPaperRespByArxivId(arxivId)
+        except connector.Error as err:
+            print(err)
+            return None
+        finally:
+            self.conn.commit()
+            self.conn.close()
+
+    def getPaperByTitle(self, query: str):
+        try:
+            return self._getPaperByTitle(query)
+        except connector.Error as err:
+            print(err)
+            return None
+        finally:
+            self.conn.commit()
+            self.conn.close()
+
+if __name__ == '__main__':
+    helper = MysqlHelper()
+    result = helper.getPaperByTitle("Attention")
+    print(result)
---
+++ SearchLambda/WeaviateHelper.py
@@ -0,0 +1,15 @@
+import weaviate
+
+
+def WeaviateHelper:
+    def __init__(self, url="https://dl2-6ld8p0k2.weaviate.network", key="ugyXUyDB0ODDjlivf1URI9lQBCQEc1t8orke", ):
+        self.client = weaviate.Client(url=url, auth_client_secret=weaviate.AuthApiKey(api_key=key), additional_headers={'X-HuggingFace-Api-Key':"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"})
+
+    def getNear(self, query):
+        return self.client.query.get("Abstract",["arxiv_id"]).with_near_text({"concepts": [query]}).with_limit(20).do()
+
+
+
+if __name__ == '__main__':
+    helper = WeaviateHelper()
+    print(helper.getNear("Attention is all you need"))
