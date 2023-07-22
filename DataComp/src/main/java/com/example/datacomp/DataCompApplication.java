b72618ea13394994bdfe068489695cc5510d19d4
Jiseshen <1658288901@qq.com>
2023-07-22 16:22:28 +0800

    DataComp

---
+++ DataComp/src/main/java/com/example/datacomp/DataCompApplication.java
@@ -0,0 +1,13 @@
+package com.example.datacomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCompApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCompApplication.class, args);
+    }
+
+}
---
+++ DataComp/src/test/java/com/example/datacomp/DataCompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datacomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCompApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
